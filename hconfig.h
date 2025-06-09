e377520a13af313f74f9cd1f9c52253bdd9c7571
mindChickey <w453908766@126.com>
2025-06-09 15:14:54 +0800

    use clang thin lto

--- hconfig.h
+++ hconfig.h
@@ -10,7 +10,7 @@
 #endif
 
 #ifndef HAVE_STDATOMIC_H
-#define HAVE_STDATOMIC_H 0
+#define HAVE_STDATOMIC_H 1
 #endif
 
 #ifndef HAVE_SYS_TYPES_H
@@ -62,15 +62,15 @@
 #endif
 
 #ifndef HAVE_PTHREAD_SPIN_LOCK
-#define HAVE_PTHREAD_SPIN_LOCK 0
+#define HAVE_PTHREAD_SPIN_LOCK 1
 #endif
 
 #ifndef HAVE_PTHREAD_MUTEX_TIMEDLOCK
-#define HAVE_PTHREAD_MUTEX_TIMEDLOCK 0
+#define HAVE_PTHREAD_MUTEX_TIMEDLOCK 1
 #endif
 
 #ifndef HAVE_SEM_TIMEDWAIT
-#define HAVE_SEM_TIMEDWAIT 0
+#define HAVE_SEM_TIMEDWAIT 1
 #endif
 
 #ifndef HAVE_PIPE
@@ -91,12 +91,12 @@
 
 /* #undef WITH_OPENSSL */
 /* #undef WITH_GNUTLS */
-/* #undef WITH_MBEDTLS */
+#define WITH_MBEDTLS   1
 
 /* #undef ENABLE_UDS */
 /* #undef USE_MULTIMAP */
 
-#define WITH_WEPOLL     1
+/* #undef WITH_WEPOLL */
 /* #undef WITH_KCP */
 
 #endif // HV_CONFIG_H_
